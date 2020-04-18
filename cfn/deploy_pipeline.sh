d4afe5fc3606f39054f84d5c9b0812a36c962da9
ddunford <djdunford@gmail.com>
2020-04-18 07:45:27 +0100

    updates to enable use of IntelliJ CloudFormation plugin

--- cfn/deploy_pipeline.sh
+++ cfn/deploy_pipeline.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-aws cloudformation update-stack --stack-name ledstrip-pipeline-s3infra --template-body file://infra_s3pipeline.yml --parameters file://param_s3pipeline.json --capabilities CAPABILITY_NAMED_IAM --profile ledstrip_admin
+aws cloudformation create-stack --stack-name ledstrip-pipeline --template-body file://infra_s3pipeline.yaml --parameters file://param_s3pipeline.json --capabilities CAPABILITY_NAMED_IAM --profile awsapp_admin
